6661e3148af7f7646c030dab6f6a0a51e3c56df2
Manmeet Singh <manmeet20singh11@gmail.com>
2021-03-23 14:34:12 -0500

    updated the compute_tci function

--- xarray_tutorial/compute_terrestrial_coupling_index.py
+++ xarray_tutorial/compute_terrestrial_coupling_index.py
@@ -6,21 +6,21 @@
 # Calculate the slope between soil moisture and surface flux (either sensible or latent) with soil moisture as the independent variable.
 # Multiple the standard deviation by the slope returning the terrestrial coupling index with the same units as the surface flux variable.
 
-def linear_trend(x, y):
-    if np.sum(np.isnan(x))>0:
-        pf = np.empty((x.shape[0]))
-    else:
-    #idx = np.isfinite(x) & np.isfinite(y)
-        pf = np.polyfit(x, y, 1)
-    return xr.DataArray(pf[0])
-
 def compute_tci(sm, lh):
+    print(sm.shape, lh.shape)
     sm_std = sm.std(dim='time')
-    slopes = xr.apply_ufunc(linear_trend,
-                            sm, lh,
-                            vectorize=True,
-                            input_core_dims=[['time'], ['time']],# reduce along 'plev'
-                            )
-    tci = slopes*sm_std
+    slopes = np.zeros_like(sm_std.values)
+
+    for i_tci in range(sm.values.shape[1]):
+        for j_tci in range(sm.values.shape[2]):
+            mask = ~np.isnan(sm.values[:,i_tci,j_tci]) & ~np.isnan(lh.values[:,i_tci,j_tci])
+            if np.sum(np.isnan(sm.values[:,i_tci,j_tci]))>=sm.values.shape[0]-2:
+                slope=np.nan
+            else:
+                slope, intercept, r_value, p_value, std_err = stats.linregress(sm.values[:,i_tci,j_tci][mask], lh.values[:,i_tci,j_tci][mask])
+            slopes[i_tci,j_tci] = slope
+        
+
+    sm_std['slopes'] = (('lat', 'lon'), slopes)
+    tci = sm_std.slopes*sm_std
     return tci
-#mrsos_control[0].std(dim='time').plot()
