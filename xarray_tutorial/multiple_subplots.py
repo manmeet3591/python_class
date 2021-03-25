7f8926bbff14a07539cd36153ef7b7ec8fe98fdb
Manmeet Singh <manmeet20singh11@gmail.com>
2021-03-25 01:19:18 -0500

    update multiple_subplots.py

--- xarray_tutorial/multiple_subplots.py
+++ xarray_tutorial/multiple_subplots.py
@@ -27,3 +27,28 @@ fig.delaxes(ax[2,2])
 fig.patch.set_linewidth(10)
 fig.patch.set_edgecolor('cornflowerblue')
 fig.suptitle('Terrestrial coupling index (Dirmeyer et al 2011) (AER - Control) MJJAS', size=16)
+
+
+
+###############  Also 
+
+data = [ds_trmm.r.sel(lon=slice(0,150), lat=slice(-20,60)).mean(dim='time'), \
+       ds_anthrop.sel(lon=slice(0,150), lat=slice(60,-20)).mean(dim='time'), \
+       ds_no_anthrop.sel(lon=slice(0,150), lat=slice(60,-20)).mean(dim='time')]
+
+fig,ax = plt.subplots(ncols=2,nrows=3, figsize=(11.69,8.27), subplot_kw={'projection': ccrs.PlateCarree()})
+fig.delaxes(ax[1,1])
+#ax_ = [ax1, ax2, ax3, ax4]
+tit_ = ['TRMM', 'Anthrop', 'No-Anthrop']
+cnt=0
+for i_fig in range(2):
+    for j_fig in range(2):
+        if cnt>2:
+            break
+        data[cnt].plot(ax=ax[i_fig, j_fig], cmap='BrBG', vmin=0, vmax=15)
+        ax[i_fig, j_fig].set_title(tit_[cnt])
+        ax[i_fig,j_fig].coastlines()
+        cnt=cnt+1
+
+fig.patch.set_linewidth(10)
+fig.patch.set_edgecolor('cornflowerblue')
