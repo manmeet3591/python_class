bd1606ac87ef678296402370099bc51486777e89
Manmeet Singh <manmeet20singh11@gmail.com>
2021-04-07 13:59:12 -0500

    create dataset

--- xarray_tutorial/create_xarray_dataarray.py
+++ xarray_tutorial/create_xarray_dataarray.py
@@ -6,3 +6,15 @@ data = np.random.rand(1, 4, 3)
 lats = np.arange(0,4,1)
 lons = np.arange(0,3,1)
 foo = xr.DataArray(data, coords=[times, lats, lons], dims=["time", "lat", "lon"])
+ds = xr.Dataset({
+    'ndvi': xr.DataArray(
+                data   = data,   # enter data here
+                dims   = ['time'],
+                coords = {'time': times},
+                attrs  = {
+                    '_FillValue': -999.9,
+                    'units'     : 'W/m2'
+                    }
+                ),
+            attrs = {'example_attr': 'this is a global attribute'}
+    )
