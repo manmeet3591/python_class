2112f63416749f0d4fa525100ebc9578ae14a291
Manmeet Singh <manmeet20singh11@gmail.com>
2021-04-07 14:16:08 -0500

    update xarray dataset

--- xarray_tutorial/create_xarray_dataarray.py
+++ xarray_tutorial/create_xarray_dataarray.py
@@ -7,8 +7,8 @@ lats = np.arange(0,4,1)
 lons = np.arange(0,3,1)
 foo = xr.DataArray(data, coords=[times, lats, lons], dims=["time", "lat", "lon"])
 ds = xr.Dataset({
-    'ndvi': xr.DataArray(
-                data   = data,   # enter data here
+    'SWdown': xr.DataArray(
+                data   = np.random.random(6),   # enter data here
                 dims   = ['time'],
                 coords = {'time': times},
                 attrs  = {
@@ -16,5 +16,15 @@ ds = xr.Dataset({
                     'units'     : 'W/m2'
                     }
                 ),
-            attrs = {'example_attr': 'this is a global attribute'}
+    'LWdown': xr.DataArray(
+                data   = np.random.random(6),   # enter data here
+                dims   = ['time'],
+                coords = {'time': times},
+                attrs  = {
+                    '_FillValue': -999.9,
+                    'units'     : 'W/m2'
+                    }
+                )
+            },
+        attrs = {'example_attr': 'this is a global attribute'}
     )
