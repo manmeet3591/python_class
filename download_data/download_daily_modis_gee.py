56f5e31582a0fc916cefd7b891c21e82033eefea
Manmeet Singh <manmeet20singh11@gmail.com>
2021-10-06 17:21:13 -0500

    Add files via upload

--- download_data/download_daily_modis_gee.py
+++ download_data/download_daily_modis_gee.py
@@ -4,8 +4,10 @@ ee.Initialize()
 import  xarray as xr
 #data_dir = '/home/cccr/msingh/data/merra_aod_global_1980_2020/MERRA2_100.tavg1_2d_aer_Nx.1981_monmean.nc4'
 import sys
-dates = sys.argv[1]# '2020-03-23'
-datee = sys.argv[2]# '2020-04-24'
+#dates = sys.argv[1]# '2020-03-23'
+#datee = sys.argv[2]# '2020-04-24'
+dates = '2020-03-23'
+datee = '2020-04-24'
 #ds_merra2 = xr.open_dataset(data_dir)
 collection_evi = ee.ImageCollection('MODIS/MYD09GA_006_EVI').select('EVI').filterDate(dates,datee ).mean()
 collection_ndsi = ee.ImageCollection('MODIS/MYD09GA_006_NDSI').select('NDSI').filterDate(dates,datee ).mean()
@@ -18,7 +20,7 @@ lats = np.arange(10,35,0.25)
 lons = np.arange(70,90,0.25)
 #print(lats.shape, lons.shape, ds_merra2.TOTEXTTAU.values.shape)
 evi = np.zeros((1, lats.shape[0], lons.shape[0]))
-ndsi = np.zeros(1, (lats.shape[0], lons.shape[0]))
+ndsi = np.zeros((1, lats.shape[0], lons.shape[0]))
 ndwi = np.zeros((1, lats.shape[0], lons.shape[0]))
 ndvi = np.zeros((1, lats.shape[0], lons.shape[0]))
 
@@ -37,7 +39,7 @@ for i_lat in range(lats.shape[0]):
     ndwi[0, i_lat, j_lon] = ee.Number(data).getInfo()
     data = collection_ndvi.reduceRegion(ee.Reducer.mean(),p,25000).get("NDVI")# 0.5 degree = 50km =50000
     ndvi[0, i_lat, j_lon] = ee.Number(data).getInfo()
-    #print(lats[i_lat], lons[j_lon], ndvi[i_lat, j_lon])
+    print(lats[i_lat], lons[j_lon], ndvi[i_lat, j_lon])
 
 times = pd.date_range(dates, periods=1)
 ds = xr.DataArray(evi, coords=[times, lats, lons], dims=["time", "lat", "lon"])
